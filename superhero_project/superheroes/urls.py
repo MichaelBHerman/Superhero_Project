51468488ba3afba5fa70c08cdb9698d876f77b1d
MichaelBHerman <michaelbherman@gmail.com>
2021-09-26 15:38:50 -0600

    Details page and Create page added

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-
+from django.urls.resolvers import URLPattern
 from . import views
 
 app_name = 'superheroes'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create'),
+    # path('edit<int:hero_id>/', views.edit, name='edit'),
+    # path('delete<int:hero_id>/', views.delete, name='delete'),
 ]
